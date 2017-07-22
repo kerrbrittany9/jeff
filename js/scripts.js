c58e2baf80ad34b1e51c8787850c3e26451208ea
brittany kerr <kerrbrittany9@gmail.com>
2017-07-22 12:25:26 -0700

    make card

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+  $("#unislice").hover(function(){
+    $("#unicorn").toggle();
+    $("#slice").toggle();
+  });
+
+  $("#question").click(function(){
+    $("#question").hide();
+    $("#").fadeIn(1500);
+
+  });
+  $("#grammy").click(function(){
+    $("#grammy").hide();
+    $("#emeline").fadeIn(1500);
+
+  });
+  $("#emeline").click(function(){
+    $("#emeline").hide();
+    $("#daddy").fadeIn(1500);
+
+  });
+});
